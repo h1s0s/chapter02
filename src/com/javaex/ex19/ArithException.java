698460f8b92e2eafa1bf25298627240f7e82a260
한상선 <hs94122@naver.com>
2021-12-02 11:17:18 +0900

    2021-12-02 11:17

--- src/com/javaex/ex19/ArithException.java
+++ src/com/javaex/ex19/ArithException.java
@@ -1,10 +1,19 @@
 package com.javaex.ex19;
 
+import java.util.Scanner;
+
 public class ArithException {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		double result;
+		int num;
+		
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		
+		
+		sc.close();
 	}
 
 }
